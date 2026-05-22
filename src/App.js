c5f17c9b0f988ff74ab4f675cd3a60ca12e507b5
balamurali55 <98606145+balamurali55@users.noreply.github.com>
2026-05-22 12:12:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Auction from './components/player_auction';
+
+function App() {
+  return (
+    <div className="App">
+      <Auction />
+    </div>
+  );
+}
+
+export default App;
